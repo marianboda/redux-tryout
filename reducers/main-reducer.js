d35679e420fd7ac4501c6cb6937370ac7e5e9916
Marian Boda <marianboda@gmail.com>
2016-02-28 20:48:38 +0100

    created main reducer

---
+++ reducers/main-reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  tasks: [{title: 'task 1'}, {title: 'task 2'}, {title: 'task 3'}]
+}
+
+const reducer = (state = initialState, action) => {
+  return state
+}
+
+export default reducer
